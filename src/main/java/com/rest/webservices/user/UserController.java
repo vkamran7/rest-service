e20716d002c4d26985b36e08862844ac684a5f61
Kamran <vkamran7@gmail.com>
2020-05-09 17:06:12 +0400

    Added UserController

---
+++ src/main/java/com/rest/webservices/user/UserController.java
@@ -0,0 +1,26 @@
+package com.rest.webservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User findById(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+}
