621644fc5908317276351fdd592a289622b6fb85
Kamran <vkamran7@gmail.com>
2020-05-10 19:45:04 +0400

    Implemented localization with LocaleResolver

--- src/main/java/com/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/rest/webservices/RestfulWebServicesApplication.java
@@ -2,6 +2,10 @@ package com.rest.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +14,10 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 }
--- src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
@@ -1,10 +1,17 @@
 package com.rest.webservices.helloworld;
 
 import com.rest.webservices.helloworld.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -19,5 +26,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-var/{name}")
     public HelloWorldBean getBean2(@PathVariable String name) {
         return new HelloWorldBean(String.format("Salam, %s qaqa", name));
+    }
+
+    @GetMapping("/sik")
+    public String helloWorldInt() {
+         return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
