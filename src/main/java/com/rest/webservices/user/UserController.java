c529ebd78f59eb028848315110c5a83f5571b0d9
Kamran <vkamran7@gmail.com>
2020-05-10 15:36:55 +0400

    Added "exception" package Added custom "ExceptionResponse" class to "exception" package Added "CustomizedResponseEntityExceptionHandler" to "exception"

--- src/main/java/com/rest/webservices/user/UserController.java
+++ src/main/java/com/rest/webservices/user/UserController.java
@@ -1,8 +1,11 @@
 package com.rest.webservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,12 +21,24 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public User findById(@PathVariable int id) {
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user == null) {
+            throw new UserNotFoundException("id -" + id);
+        }
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+        URI location =  ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+
     }
 
 }
