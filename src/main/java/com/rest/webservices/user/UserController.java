23de487325a0efaeef59876efa2fd341001ec253
Kamran <vkamran7@gmail.com>
2020-05-10 20:18:56 +0400

    Implemented Swagger Documentation

--- src/main/java/com/rest/webservices/user/UserController.java
+++ src/main/java/com/rest/webservices/user/UserController.java
@@ -1,10 +1,11 @@
 package com.rest.webservices.user;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+//import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import com.rest.webservices.exception.UserNotFoundException;
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+//import org.springframework.hateoas.EntityModel;
+//import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -24,16 +25,25 @@ public class UserController {
         return service.findAll();
     }
 
+//    @GetMapping("/users/{id}")
+//    public EntityModel<User> findById(@PathVariable int id) {
+//        User user = service.findOne(id);
+//        if(user == null) {
+//            throw new UserNotFoundException("id -" + id);
+//        }
+//        EntityModel<User> resource = new EntityModel<>(user);
+//        WebMvcLinkBuilder linkTo =  linkTo(methodOn(this.getClass()).retrieveAllUsers());
+//        resource.add(linkTo.withRel("all-users"));
+//        return resource;
+//    }
+
     @GetMapping("/users/{id}")
-    public EntityModel<User> findById(@PathVariable int id) {
+    public User findById(@PathVariable int id) {
         User user = service.findOne(id);
         if(user == null) {
             throw new UserNotFoundException("id -" + id);
         }
-        EntityModel<User> resource = new EntityModel<>(user);
-        WebMvcLinkBuilder linkTo =  linkTo(methodOn(this.getClass()).retrieveAllUsers());
-        resource.add(linkTo.withRel("all-users"));
-        return resource;
+        return user;
     }
 
     @PostMapping("/users")
