068d90172159c9e36ea2d79a6d44c8d7cf4bf042
Kamran <vkamran7@gmail.com>
2020-05-11 06:18:38 +0400

    Added Actuator and Hal Browser Commented all Swagger related code due to conflicts

--- src/main/java/com/rest/webservices/user/User.java
+++ src/main/java/com/rest/webservices/user/User.java
@@ -1,22 +1,22 @@
 package com.rest.webservices.user;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+//import io.swagger.annotations.ApiModel;
+//import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
-@ApiModel(description = "All details about user")
+//@ApiModel(description = "All details about user")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "en az 2 zad qoyginen")
-    @ApiModelProperty(notes = "name should be minimum 2 characters")
+//    @ApiModelProperty(notes = "name should be minimum 2 characters")
     private String name;
 
     @Past
-    @ApiModelProperty(notes = "Birth date should be in the past")
+//    @ApiModelProperty(notes = "Birth date should be in the past")
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
