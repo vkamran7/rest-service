016274b83f946691f66e926e55388a113dd8fcef
Kamran <vkamran7@gmail.com>
2020-05-14 16:26:58 +0400

    Creating Post Entity and Many to One Relationship with User Entity

--- src/main/java/com/rest/webservices/user/User.java
+++ src/main/java/com/rest/webservices/user/User.java
@@ -3,13 +3,11 @@ package com.rest.webservices.user;
 //import io.swagger.annotations.ApiModel;
 //import io.swagger.annotations.ApiModelProperty;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 //@ApiModel(description = "All details about user")
 @Entity
@@ -27,6 +25,9 @@ public class User {
 //    @ApiModelProperty(notes = "Birth date should be in the past")
     private Date birthDate;
 
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
     public User(Integer id, String name, Date birthDate) {
         this.id = id;
         this.name = name;
@@ -35,15 +36,6 @@ public class User {
 
     public User() {
 
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", birthDate=" + birthDate +
-                '}';
     }
 
     public Integer getId() {
@@ -68,5 +60,22 @@ public class User {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
     }
 }
